69cb12ea2459387b004ac7153cb60e6257804285
Alejandro Alonso <aalonsopuig@gmail.com>
2023-11-12 20:03:27 +0100

    update

--- src/l_swap_panel/MainClass.java
+++ src/l_swap_panel/MainClass.java
@@ -1,5 +1,19 @@
 package l_swap_panel;
 
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+
 public class MainClass {
-    
+ 
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Ejemplo Listeners");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.setBounds(100, 100, 400, 400);
+
+        JComponent newContentPane = new SwapingPanel();
+        newContentPane.setOpaque(true); frame.setContentPane(newContentPane);
+        
+        frame.setVisible(true);
+    }    
 }
